f5d963d905ffbaa5a971ced61943c1a1e636dfb4
Chistry <christian.diaz@correo.unimet.edu.ve>
2024-03-09 01:46:09 -0400

    esqueleto homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@
 
 import './App.css'
 import Header from './Header/Header.jsx'
+import HomePage from './Home-Page/homepage.jsx';
 
 function App() {
 
   return (
     <div>
         <Header />
+        <HomePage />
     </div>
   )
 }
 
 export default App
+
